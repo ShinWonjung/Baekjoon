8d8a07cad54a05a60fe486f8a8d40db9c1729006
원정 신 <dnjswjd1204@naver.com>
2021-09-22 22:32:18 +0900

    commit

---
+++ nm/nm/소스.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	vector<int>vec{ 0 };
+	for (int i = 1; i <= n; i++)
+		vec.push_back(i);
+	for (int i = 1; i <= n; i++)
+	{
+		for(int j=i)
+	}
+}
