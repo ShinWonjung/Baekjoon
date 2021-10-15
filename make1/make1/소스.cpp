97f0a1aeada6a0bda1358682ec0487ad3822a738
ShinWonjung <dnjswjd1204@gmail.com>
2021-10-15 21:21:10 +0900

    commit

--- make1/make1/소스.cpp
+++ make1/make1/소스.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
+
 int main()
 {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
 	int n;
-	int result = 0;
 	cin >> n;
-	while (n != 1)
+	vector<int>vec(n + 1, 1);
+	vec[1] = 0;
+	for (int i = 4; i <= n; i++)
 	{
-		
+		vec[i] = vec[i - 1] + 1;
+		if (!(i % 3)) vec[i] = min(vec[i], vec[i / 3] + 1);
+		if (!(i % 2)) vec[i] = min(vec[i], vec[i / 2] + 1);
 	}
-	cout << result;
+	cout << vec[n];
 	return 0;
 }
