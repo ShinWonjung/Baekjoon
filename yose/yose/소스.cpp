0ee0e0ee1b1b2b90be81f2af2745bb0b8394c116
ShinWonjung <dnjswjd1204@gmail.com>
2021-10-25 21:24:25 +0900

    commit

---
+++ yose/yose/소스.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+	queue<int>q;
+	for (int i = 1; i <= n; i++) q.push(i);
+	int i = 1;
+	cout << '<';
+	while (q.size() > 1) {
+		if (i % k == 0) {
+			cout << q.front() << ", ";
+		}
+		else {
+			q.push(q.front());
+		}
+		q.pop();
+		i++;
+	}
+	cout << q.front() << '>';
+	return 0;
+}
