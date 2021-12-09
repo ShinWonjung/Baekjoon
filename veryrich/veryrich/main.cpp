e6ad819db5fbd0c8b124d3b14625925bbe4ac6b8
ShinWonjung <dnjswjd1204@gmail.com>
2021-12-09 15:42:39 +0900

    commit

---
+++ veryrich/veryrich/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  veryrich
+//
+//  Created by 신원정 on 2021/12/09.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int n, m;
+    cin>>n>>m;
+    cout<<n/m<<'\n'<<n%m;
+    return 0;
+}
